4c02c6823ce34059663bf72f757a28f52578dfe4
Joe Tiersma <joe.tiersma@gmail.com>
2019-12-12 07:40:54 -0600

    Updated npm and installed moment.js. Changed tab icon and app title.

---
+++ src/components/Calendar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import moment from 'moment'
+
+class Calendar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Calendar
